6a17f93ed8da401012dee6e1205decf6e03036d5
Suresh-03 <sureshkumar732003@gmail.com>
2023-10-18 21:14:47 +0530

    new

---
+++ maxOfAllSubArraySizeK.java
@@ -0,0 +1,15 @@
+public class maxOfAllSubArraySizeK {
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        // Your code here
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i <= n-k; i++){
+            int max = arr[i];
+            for(int j = i; j < i+k; j++){
+                max = Math.max(max,arr[j]);
+            }
+            list.add(max);
+        }
+        return list;
+    }
+}
